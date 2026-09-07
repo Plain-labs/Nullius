8210a85e6aad7d0aba541594288a54ad868e1248
IyanuOluwaJesuloba <jesulobaowoseni1@gmail.com>
2026-09-07 13:52:04 +0100

    chore: remove build artifacts, fix language stats, rewrite docs

--- target/wasm32-unknown-unknown/release/deps/payment_gate.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dell\Documents\web3\ZK\target\wasm32-unknown-unknown\release\deps\payment_gate.d: contracts\payment_gate\src\lib.rs
-
-C:\Users\dell\Documents\web3\ZK\target\wasm32-unknown-unknown\release\deps\payment_gate.wasm: contracts\payment_gate\src\lib.rs
-
-contracts\payment_gate\src\lib.rs:
--- target/wasm32-unknown-unknown/release/deps/reputation_registry.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dell\Documents\web3\ZK\target\wasm32-unknown-unknown\release\deps\reputation_registry.d: contracts\reputation_registry\src\lib.rs
-
-C:\Users\dell\Documents\web3\ZK\target\wasm32-unknown-unknown\release\deps\reputation_registry.wasm: contracts\reputation_registry\src\lib.rs
-
-contracts\reputation_registry\src\lib.rs:
